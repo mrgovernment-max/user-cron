def54deacea37a694c6ae4e0d8df3d5f41ebe4c9
Emman <efenteng1@gmail.com>
2025-09-05 13:54:50 +0100

    ppp

--- updateBalances.js
+++ updateBalances.js
@@ -3,10 +3,10 @@ const nodemailer = require("nodemailer");
 
 // ✅ MySQL Pool
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "sql8.freesqldatabase.com",
-  user: process.env.DB_USER || "sql8792916",
-  password: process.env.DB_PASS || "iEdb2pFif4",
-  database: process.env.DB_NAME || "sql8792916",
+  host: "sql8.freesqldatabase.com",
+  user: "sql8792916",
+  password: "iEdb2pFif4",
+  database: "sql8792916",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
